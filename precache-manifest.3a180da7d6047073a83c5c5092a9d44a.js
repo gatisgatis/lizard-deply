7b7ae8699e45351b5de676f25107a9aca8f3be08
Gatis <gatis.meikulis@gmail.com>
2021-01-14 21:52:57 +0200

    deploy

---
+++ precache-manifest.3a180da7d6047073a83c5c5092a9d44a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70af58b87ef4685c83f",
+    "url": "/lizard-deply/css/app.8649e2a7.css"
+  },
+  {
+    "revision": "c4c732b1dab9cb82cabc72f6dc1a22a8",
+    "url": "/lizard-deply/img/fire.c4c732b1.jpg"
+  },
+  {
+    "revision": "48a69864c5198cf24a3252f381dfc690",
+    "url": "/lizard-deply/img/lizard.48a69864.png"
+  },
+  {
+    "revision": "7121604187a3a5ae9c8ac23c5e04a0ce",
+    "url": "/lizard-deply/img/logo.71216041.jpg"
+  },
+  {
+    "revision": "e6caf672cffdbf4f741f466072518cab",
+    "url": "/lizard-deply/img/paper.e6caf672.png"
+  },
+  {
+    "revision": "7f571ede1b0e9ca9fc35089b25a6294f",
+    "url": "/lizard-deply/img/question.7f571ede.png"
+  },
+  {
+    "revision": "8ce213cb910d8afe9703efae5b328e99",
+    "url": "/lizard-deply/img/rock.8ce213cb.png"
+  },
+  {
+    "revision": "e99a572fdf1308f7477061e5978ee5d6",
+    "url": "/lizard-deply/img/rules.e99a572f.png"
+  },
+  {
+    "revision": "29364dfe48673606e4ab0c7f102bbd29",
+    "url": "/lizard-deply/img/scissors.29364dfe.png"
+  },
+  {
+    "revision": "c5cc86113f7aea52260b8fbc9b44db3c",
+    "url": "/lizard-deply/img/spock.c5cc8611.png"
+  },
+  {
+    "revision": "debf3691c46d710458a69db108eef3dc",
+    "url": "/lizard-deply/img/title.debf3691.jpg"
+  },
+  {
+    "revision": "38fe7abc33723153742374ca695cd1d0",
+    "url": "/lizard-deply/index.html"
+  },
+  {
+    "revision": "d70af58b87ef4685c83f",
+    "url": "/lizard-deply/js/app.efb23cfc.js"
+  },
+  {
+    "revision": "c50bb05f51c2cb239fe6",
+    "url": "/lizard-deply/js/chunk-vendors.aa1d55e5.js"
+  },
+  {
+    "revision": "52c259e532f16714307094a993794409",
+    "url": "/lizard-deply/manifest.json"
+  },
+  {
+    "revision": "1f042630a552a96f8145869af4877703",
+    "url": "/lizard-deply/media/fire.1f042630.mp3"
+  },
+  {
+    "revision": "2c96c686ddd8e6c7d291245d2ead8ba6",
+    "url": "/lizard-deply/media/laughs.2c96c686.mp3"
+  },
+  {
+    "revision": "3364fceb2705c8b0c45a3943605038fa",
+    "url": "/lizard-deply/media/tie.3364fceb.mp3"
+  },
+  {
+    "revision": "9f537766aea17e364b7ea13980f36f6e",
+    "url": "/lizard-deply/media/victory.9f537766.mp3"
+  },
+  {
+    "revision": "ca99e7e0dbc12d27c74ddddf95f82c92",
+    "url": "/lizard-deply/media/walking.ca99e7e0.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lizard-deply/robots.txt"
+  }
+]);
